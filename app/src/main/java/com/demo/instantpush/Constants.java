25b2404556d48bb5145f28b1b25b141d7f60fc97
Sneha010 <sneha.khadatare@gmail.com>
2016-07-04 15:58:46 +0530

    token received

--- app/src/main/java/com/demo/instantpush/Constants.java
+++ app/src/main/java/com/demo/instantpush/Constants.java
@@ -7,7 +7,7 @@ package com.demo.instantpush;
 
 public class Constants {
 
-    public static final String BASE_URL = "ADD BASE URL HERE";
-    public static final String PROJECT_ID = "ADD YOUR PROJECT ID HERE";
-    public static final String DEVICE_ID = "ADD YOUR DEVICE UNIQUE ID HERE";
+    public static final String BASE_URL = "https://push.tcsmarathonapp.com/MarathonPushTest/";
+    public static final String PROJECT_ID = "322419976239";
+    public static final String DEVICE_ID = "352136062659210";
 }
